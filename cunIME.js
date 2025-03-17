9569fcb96d7bbf893f055fcf2340cbf440829a46
panglossa <panglossa@yahoo.com.br>
2025-03-17 13:23:49 -0300

    major update
    added syllable builder input, put each input type (strokes, syllabic, reading, meaning) in a separate tab

--- cunIME.js
+++ cunIME.js
@@ -1,6 +1,9 @@
 var strokes = [];
 var glyphdata = [];
+var sumerakkaddata = [];
 var favourites = [];
+var cvcdata = [];
+var cvc = ['Ø', 'a', 'Ø'];
 
 csv = '';
 fetch('./cunIMEglyphdata.csv')
@@ -18,6 +21,42 @@ fetch('./cunIMEglyphdata.csv')
 		i++;
 		} ); 
 	//console.log(glyphdata);
+  });
+
+csv = '';
+fetch('./cunIMEglyphSumerAkkad.csv')
+  .then(response => response.text())
+  .then((data) => {
+  	const rows = data.split('\n');
+  	let i=1;
+	rows.forEach(function (row) {
+		parts = row.split(',');
+		if (parts.length<2) {
+			console.log('Wrong format in line ' + i + ': ' + row);
+			} else {
+			sumerakkaddata.push(parts);
+			}
+		i++;
+		} ); 
+	//console.log(glyphdata);
+  });
+
+csv = '';
+fetch('./CVC.csv')
+  .then(response => response.text())
+  .then((data) => {
+  	const rows = data.split('\n');
+  	let i=1;
+	rows.forEach(function (row) {
+		parts = row.split(',');
+		if (parts.length<2) {
+			console.log('Wrong format in line ' + i + ': ' + row);
+			} else {
+			cvcdata.push([parts[0], parts[1].split(';')]);
+			}
+		i++;
+		} ); 
+	console.log(cvcdata);
   });
   
 favourites = localStorage.getItem('favourites');
@@ -96,6 +135,18 @@ function updatecandidates(){
 	buffer = document.getElementById('buffer');
 	candidates = document.getElementById('candidates');
 	candidates.innerHTML = '';
+	check = '';
+	finished = false;
+	for (i = 0; i <strokes.length; i++) {
+		if (!finished) {
+			if (strokes[i]=='finished') {
+				finished = true;
+				} else {
+				check += strokes[i];
+				}
+			}
+		}
+	
 	if (glyphdata.length > 0) {
 	glyphdata.forEach(function(row) {
 		ismatch = false;
@@ -104,10 +155,16 @@ function updatecandidates(){
 			}
 		parts = row[1].split(';');
 		parts.forEach(function(part) {
-			if (part.startsWith(strokes.join(''))) {
-			//if (part.startsWith(buffer.innerHTML)) {
-				ismatch = true;
+			if (finished) {
+				if (part==check) {
+					ismatch = true;
+					}
+				} else {
+				if (part.startsWith(check)) {
+					ismatch = true;
+					}
 				}
+			
 			});
     if (ismatch) {
     	candidates.innerHTML += ' ' + '<a title="Click to insert this character" href="#" onclick="javascript:insertglyph(' + "'" + row[0] + "'" + ');return false;">' + row[0] + '</a>';
@@ -117,57 +174,64 @@ function updatecandidates(){
 	}
 
 function addstroke(s, q = 1) {
-	buffer = document.getElementById('buffer');
-	for (let i = 1; i<=q; i++) {
-		if (s=='horizontalUwedge') {
-			buffer.innerHTML += '<div class="horizontalwedge"><img /></div><div class="Uwedge"><img /></div>';
-			} else {
-			buffer.innerHTML += '<div class="' + s + '"><img /></div>';
-			}
-		}
-	
-	newstroke = '';
-	switch(s) {
-		case 'horizontalwedge':
-			newstroke = '𒀸';
-			break;
-		case 'verticalwedge':
-			newstroke = '𒁹';
-			break;
-		case 'verticalinvertwedge':
-			newstroke = '𝆹𝅥';
-			break;
-		case 'diagwedgeNWSE':
-			newstroke = '𒀹';
-			break;
-		case 'diagwedgeSENW':
-			newstroke = '𒀺';
-			break;
-		case 'diagwedgeSWNE':
-			newstroke = '𒍻';
-			break;
-		case 'diagwedgeNESW':
-			newstroke = '🝯';
-			break;
-		case 'Uwedge':
-			newstroke = '𒌋';
-			break;
-		case 'horizontalUwedge':
-			newstroke = '𒀸𒌋';
-			break;
-		}
+	if (!strokes.includes('finished')) {
+		buffer = document.getElementById('buffer');
+	if (s=='finished') {
+		strokes.push(s);
+		buffer.innerHTML += '<div class=""> ✅ </div>';
+		} else {
 		
-	for (let i = 1; i<=q; i++) {
-		if (newstroke=='𒀸𒌋') {
-			strokes.push('𒀸');
-			strokes.push('𒌋');
-			} else {
-			strokes.push(newstroke);
+		for (let i = 1; i<=q; i++) {
+			if (s=='horizontalUwedge') {
+				buffer.innerHTML += '<div class="horizontalwedge"><img /></div><div class="Uwedge"><img /></div>';
+				} else {
+				buffer.innerHTML += '<div class="' + s + '"><img /></div>';
+				}
+			}
+		
+		newstroke = '';
+		switch(s) {
+			case 'horizontalwedge':
+				newstroke = '𒀸';
+				break;
+			case 'verticalwedge':
+				newstroke = '𒁹';
+				break;
+			case 'verticalinvertwedge':
+				newstroke = '𝆹𝅥';
+				break;
+			case 'diagwedgeNWSE':
+				newstroke = '𒀹';
+				break;
+			case 'diagwedgeSENW':
+				newstroke = '𒀺';
+				break;
+			case 'diagwedgeSWNE':
+				newstroke = '𒍻';
+				break;
+			case 'diagwedgeNESW':
+				newstroke = '🝯';
+				break;
+			case 'Uwedge':
+				newstroke = '𒌋';
+				break;
+			case 'horizontalUwedge':
+				newstroke = '𒀸𒌋';
+				break;
+			}
+			
+		for (let i = 1; i<=q; i++) {
+			if (newstroke=='𒀸𒌋') {
+				strokes.push('𒀸');
+				strokes.push('𒌋');
+				} else {
+				strokes.push(newstroke);
+				}
 			}
 		}
 	updatecandidates();
 	}
-	
+	}
 function backspace() {
 	strokes.pop();
 	buffer = document.getElementById('buffer');
@@ -212,6 +276,10 @@ function clearall(){
 	strokes = [];
 	candidates = document.getElementById('candidates');
 	candidates.innerHTML = '';
+	semantic = document.getElementById('semantic');
+	semantic.value = '';
+	phonetic = document.getElementById('phonetic');
+	phonetic.value = '';
 	}
 
 function cleartext(){
@@ -245,7 +313,190 @@ function selectfont(fontname) {
 	document.querySelector('#candidates').style.fontFamily = fontname;
 	document.querySelector('#favouriteglyphs').style.fontFamily = fontname;
 	}
+
+function searchsemantic(){
+	buffer = document.getElementById('buffer_semantic');
+	candidates = document.getElementById('candidates_semantic');
+	semantic = document.getElementById('semantic');
+	candidates.innerHTML = '';
+	if (sumerakkaddata.length > 0) {
+	sumerakkaddata.forEach(function(row) {
+		ismatch = false;
+		if (row.length<2) {
+			console.log(row);
+			}
+		if (row[2]!='') {
+			parts = row[2].split('|');
+			parts.forEach(function(part) {
+				if (part.toLowerCase().includes(semantic.value.toLowerCase())) {
+				//if (part.startsWith(buffer.innerHTML)) {
+					ismatch = true;
+					}
+				});
+    	}
+		if (row[4]!='') {
+			parts = row[4].split('|');
+			parts.forEach(function(part) {
+				if (part.toLowerCase().includes(semantic.value.toLowerCase())) {
+				//if (part.startsWith(buffer.innerHTML)) {
+					ismatch = true;
+					}
+				});
+    	}
+	    if (ismatch) {
+	    	candidates.innerHTML += ' ' + '<a title="Click to insert this character" href="#" onclick="javascript:insertglyph(' + "'" + row[0] + "'" + ');return false;">' + row[0] + '</a>';
+	    	}
+	});
+    	}
+	}
+
+
+
+function searchphonetic(){
+	buffer = document.getElementById('buffer_phonetic');
+	candidates = document.getElementById('candidates_phonetic');
+	phonetic = document.getElementById('phonetic');
+	candidates.innerHTML = '';
+	if (sumerakkaddata.length > 0) {
+	sumerakkaddata.forEach(function(row) {
+		ismatch = false;
+		if (row.length<2) {
+			console.log(row);
+			}
+		if (row[1]!='') {
+			parts = row[1].split('|');
+			parts.forEach(function(part) {
+				if (part.toLowerCase().includes(phonetic.value.toLowerCase())) {
+				//if (part.startsWith(buffer.innerHTML)) {
+					ismatch = true;
+					}
+				});
+    	}
+		if (row[3]!='') {
+			parts = row[3].split('|');
+			parts.forEach(function(part) {
+				if (part.toLowerCase().includes(phonetic.value.toLowerCase())) {
+				//if (part.startsWith(buffer.innerHTML)) {
+					ismatch = true;
+					}
+				});
+    	}
+	    if (ismatch) {
+	    	candidates.innerHTML += ' ' + '<a title="Click to insert this character" href="#" onclick="javascript:insertglyph(' + "'" + row[0] + "'" + ');return false;">' + row[0] + '</a>';
+	    	}
+	});
+    	}
+	}
+
+function switchtab(tabindex) {
+	var strokeinput = document.getElementById('strokeinput');
+	var syllableinput = document.getElementById('syllableinput');
+	var pronunciationinput = document.getElementById('pronunciationinput');
+	var meaninginput = document.getElementById('meaninginput');
+	strokeinput.style.display = 'none';
+	syllableinput.style.display = 'none';
+	pronunciationinput.style.display = 'none';
+	meaninginput.style.display = 'none';
+	switch(tabindex) {
+		case 0:
+			strokeinput.style.display = 'block';
+			break;
+		case 1:
+			syllableinput.style.display = 'block';
+			break;
+		case 2:
+			pronunciationinput.style.display = 'block';
+			break;
+		case 3:
+			meaninginput.style.display = 'block';
+			break;
+		}
+	}
 	
+function composesyllable(index, letter) {
+	cvc[index] = letter;
+	cvc0 = document.getElementById('cvc0');
+	cvc1 = document.getElementById('cvc1');
+	cvc2 = document.getElementById('cvc2');
+	cvc0.innerHTML = cvc[0];
+	cvc1.innerHTML = cvc[1];
+	cvc2.innerHTML = cvc[2];
+	
+	candidates = document.getElementById('candidates_cvc');
+	candidates.innerHTML = '';
+	
+	cvcdata.forEach(function (row) {
+		row[1].forEach(function(syll) {
+			match = syllablematch(syll, cvc);
+			if (match!='') {
+				candidates.innerHTML += '<a href="#" title="Click to insert this character" onclick="javascript:insertglyph(' + "'" + row[0] + "'" + ');return false;">' + row[0] + ' ' + match + '</a>' + '<br/>';
+				}
+			});
+		} ); 
+	if (cvc[2]!='Ø') {
+		getsecondarysyllables(cvc);
+		}
+	}
+	
+function getsecondarysyllables(pattern) {
+	candidates = document.getElementById('candidates_cvc');
+	
+	first = pattern[0] + pattern[1];
+	cvcdata.forEach(function (row) {
+		row[1].forEach(function(syll1) {
+			if (syll1==first) {
+				//first part found, look for second
+				cvcdata.forEach(function (row2) {
+					pattern[2].split('/').forEach(function (c2) {
+						second = pattern[1] + c2;
+						row2[1].forEach(function(syll2) {
+							if (syll2==second) {
+								candidates.innerHTML += '<a href="#" title="Click to insert this character" onclick="javascript:insertglyph(' + "'" + row[0] + ' ' + row2[0] + "'" + ');return false;">' + row[0] + ' ' + row2[0] + ' ' + first + '.' + second + '</a>' + '<br/>';
+								}
+							});
+						});
+					});
+				}
+			});
+		});
+	// cvcdata.forEach(function (row) {
+	// 	row[1].forEach(function(syll1) {
+	// 		if (syll1==first) {
+	// 			pattern[2].split('/').forEach(function (c2) {
+	// 				second = pattern[1] + c2;
+	// 				row[1].forEach(function(syll2) {
+	// 					if (syll2==second) {
+	// 						candidates.innerHTML += '<a href="#" title="Click to insert this character" onclick="javascript:insertglyph(' + "'" + syll1 + ' ' + syll2 + "'" + ');return false;">' + syll1 + ' ' + syll2 + ' ' + first + '.' + second + '</a>' + '<br/>';
+	// 						}
+	// 					});
+	// 				});
+	// 			}
+	// 		});
+	// 	});
+	}
+	
+function syllablematch(subject, pattern) {
+	res = '';
+	pattern[2].split('/').forEach(function (c2) {
+		c1 = pattern[0];
+		if (c1=='Ø') {
+			c1 = '';
+			}
+		
+		test = c1+pattern[1];
+		if (c2=='Ø') {
+			if (subject.startsWith(test)) {
+				res = subject;
+				}
+			} else {
+			if (subject==test+c2) {
+				res = subject;
+				}
+			}
+		
+		});
+	return res;
+	}
 ///////////////////////////////////////
 window.onload = function () {
 	selectedfont = localStorage.getItem('selectedfont');
@@ -262,4 +513,6 @@ window.onload = function () {
 			favouriteglyphs.innerHTML += ' ' + '<a href="#" title="Click to insert this character" onclick="javascript:insertglyph(' + "'" + favourites[i]['glyph'] + "'" + ');return false;">' + favourites[i]['glyph'] + '</a>';
 			}
 		}
+	switchtab(1);
 	}	
+	
